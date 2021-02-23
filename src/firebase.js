6d7d1e63fc4a850a8516d7f52d7ee0bb18904380
Zaibi27 <55849240+Zaibi27@users.noreply.github.com>
2021-02-23 16:47:27 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,10 @@
 import firebase from "firebase" ;
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// paste the firebase config from the firebase SDK here
 const firebaseConfig = {
-  apiKey: "AIzaSyCMlu-lou22HUUSehMVqjo79OFgYpdUDfg",
-  authDomain: "netflix-clone-ccc04.firebaseapp.com",
-  projectId: "netflix-clone-ccc04",
-  storageBucket: "netflix-clone-ccc04.appspot.com",
-  messagingSenderId: "830038149479",
-  appId: "1:830038149479:web:3cb035121deada794587c2",
-  measurementId: "G-XS16RTBMNN"
+ 
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig) ;
